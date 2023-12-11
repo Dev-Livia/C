497c9d1078a6991b7f4287d2397b3d2a2b13c717
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2023-12-11 17:12:59 -0300

    up

--- CursoEmC/Aula02.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-    // Nesta aula vamos aprender que para pularmos linhas utilizaremos o seguinte caractere \n
-    printf("\nHello guys\n how are you?");
-    return 0;
-
-}
