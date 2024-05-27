c66aae938f1db034a46aaff262e1742df9432884
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-05-27 10:03:11 -0300

    up01

---
+++ Lista1/Ex01.cpp
@@ -0,0 +1,10 @@
+	/*
+	Name: Lívia Nicole
+	Date: 27/05/24 10:00
+	Description: 1. Faça um programa que exiba seu nome na tela
+*/
+#include <stdio.h>
+int main(){
+	printf("Livia Nicole.");
+	return 0;
+}
