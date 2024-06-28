84825a999b8db7709001ce7af07054a078efa2c4
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-06-28 19:41:01 -0300

    Create Ex10.cpp

---
+++ Lista2/Ex10.cpp
@@ -0,0 +1,14 @@
+/*
+	Name: Livia Nicole
+	Date: 28/06/24 19:38
+	Description: 10.	Faça um programa que escreva os valores de 1 até 10 em ordem inversa na tela. 
+*/
+#include <stdio.h>
+int main(){
+	int num = 10;
+	for(num; num > 0; num--){
+		printf("%d\n",num);
+	}
+	return 0;
+}
+
