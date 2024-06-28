6ba94140781a7e2b0144035743316d3e315d4002
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-06-28 19:34:22 -0300

    Create Ex09.cpp

---
+++ Lista2/Ex09.cpp
@@ -0,0 +1,12 @@
+/*
+	Name:Lívia Nicole 
+	Date: 28/06/24 19:28
+	Description: 9.	Faça um programa para escrever seu nome 10 vezes na tela
+*/
+#include <stdio.h>
+int main(){
+	int c = 1;
+	for(c; c < 11; c++){
+		printf("Livia Nicole\n");
+	}
+}
