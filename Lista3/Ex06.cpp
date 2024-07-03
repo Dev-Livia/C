d767f5009d4dbbd5bfa41c3b1c16a1639adb0bc7
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-07-03 14:07:39 -0300

    Create Ex06.cpp

---
+++ Lista3/Ex06.cpp
@@ -0,0 +1,22 @@
+/*
+	Name: Lívia Nicole 
+	Date: 03/07/24 13:38
+	Description:
+	6.	Fazer um programa para exibir os números múltiplos de 11 entre 1 e 100 em ordem decrescente.  
+*/
+#include<stdio.h>
+int main(){
+	int num;
+	int inicio = 100, fim = 1;
+	num = fim;
+	
+	while(num % 11 != 0){
+		num++;
+	}
+	
+	while(num <= inicio){
+		printf("%d\n",num);
+		num += 11;
+	}
+	return 0;
+}
