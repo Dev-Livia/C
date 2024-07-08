65b1fd75a497e9ac473ec62e27b6cf21725828c7
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-07-08 19:57:47 -0300

    Create Ex01.cpp

---
+++ Lista4/Ex01.cpp
@@ -0,0 +1,17 @@
+/*
+	Name: Lívia Nicole
+	Date: 08/07/24 19:52
+	Description: RESOLVA OS EXERCÍCIOS USANDO SOMENTE O LAÇO DE REPETIÇÃO: DO WHILE. 
+ 
+1.	Faça um programa para escrever seu nome 10 vezes na tela. 
+
+*/
+#include <stdio.h>
+int main(){
+	int c = 1;
+	do{
+		printf("Lívia Nicole\n");
+		c++;
+	}while(c <= 10);
+	return 0;
+}
