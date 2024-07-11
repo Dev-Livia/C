c595260bfa572817356c5b024e90ae9b692f099d
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-07-11 15:05:23 -0300

    Create Ex07.cpp

---
+++ Lista4/Ex07.cpp
@@ -0,0 +1,7 @@
+/*
+	Name: Lívia Nicole
+	Date: 08/07/24 21:14
+	Description: 7.	Faça um programa que solicita que o usuário digite 10 valores.
+	 O programa deverá contar quantos valores estão dentro do intervalo ]10,157[ e quantos valores estão fora desse intervalo. 
+*/
+
