bc7a8a1fa42e0866eafd00b4c3f7b7bba14c94b2
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2026-03-26 21:25:26 -0300

    Calcular Média

---
+++ Media.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	
+	float N1, N2, N3;
+	float Media;
+	
+	printf("*** Programa de Cálculo de Média ***");
+	
+	printf("\nPrimeira Nota:	");
+	scanf("%f", &N1);
+
+	printf("\nSegunda Nota:		");
+	scanf("%f", &N2);
+	ss
+	printf("\nTerceira Nota:	");
+	scanf("%f", &N3);	
+	
+	Media = (float)(N1 + N2 + N3) / 3;
+	
+	printf("\nSua Média é: %2.f",Media);
+}
